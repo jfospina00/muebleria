816d3d7571a448295686a308afc7db86840a6949
Andres Rios <andres.rios.afrc@gmail.com>
2016-05-14 16:24:59 -0500

    autentificacion

--- database/migrations/2016_05_14_155506_create_users_table.php
+++ database/migrations/2016_05_14_155506_create_users_table.php
@@ -15,12 +15,12 @@ class CreateUsersTable extends Migration {
 		Schema::create('users', function(Blueprint $table)
 		{
 			$table->increments('id');
-			$table->integer('role');
-			$table->string('first_name');
-			$table->string('last_name');
-			$table->integer('telephone');
-			$table->integer('cellphone');
-			$table->string('address');
+			$table->integer('role')->nullable();
+			$table->string('first_name')->nullable();
+			$table->string('last_name')->nullable();
+			$table->integer('telephone')->nullable();
+			$table->integer('cellphone')->nullable();
+			$table->string('address')->nullable();
 			$table->string('email')->unique();
 			$table->string('password', 60);
 
